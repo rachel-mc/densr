a89cf80ddd2ec2e9c1c103b762cba3de3b603070
ConorTG <151767836+ConorTG@users.noreply.github.com>
2024-01-04 16:38:24 +0000

    Update create_hist_dens.R
    Documentation added to function create_hist_dens

--- R/create_hist_dens.R
+++ R/create_hist_dens.R
@@ -25,6 +25,41 @@
 # For ROxygen documentation:
 #@seealso See \code{\link[stats]{density}} for a complete list of arguments that can be passed when \code{type = "kde"}
 
+#' Class Constructor Function: create_hist_dens
+#'
+#' Function computes a histogram and chooses the density estimation provided by
+#' user (default=Gaussian), assigning the result to class \code{"hist_dens"}
+#'
+#' @param x A numeric vector representing the data
+#' @param type Character string specifying the type of density estimation to be
+#' used. Options are "gaussian", "kde", and "fp"
+#' @param ... Additional arguments to be passed to the density estimation
+#' function
+#'
+#' @importFrom graphics "hist"
+#' @importFrom stats "dnorm" "sd" "density"
+#'
+#' @export
+#'
+#' @family densr
+#'
+#' @author Rachel McInerney - <\email{rachel.mcinerney.2021@@mumail.ie}>
+#'         Conor Thompson - <\email{conor.thompsongargan.2020@@mumail.ie}>
+#'         Osi Ukachukwu - <\email{osi.ukachukwu.2018@@mumail.ie}>
+#'
+#'
+#' @return An object of class \code{"hist_dens"} containing the
+#' histogram (\code{h}), density values (\code{d}) and type of density
+#' estimation used (\code{type}).
+#'
+#' @seealso \code{\link[stats]{density}} for a complete list of arguments that
+#'         can be passed when \code{type = "kde"}
+#'          \code{\link{frequenxy_polygon}} to create frequency polygon density estimations 
+#'           when type specified is \code{type = "fp"}
+#' @examples
+#'        create_hist_dens(mtcars$mpg, type="kde", bw=2)
+#'        create_hist_dens(iris$Sepal.Length, type="fp")
+#'
 create_hist_dens <- function(x,
                       type = c("gaussian", "kde", "fp"), # fp is an abbreviation for frequency polygon
                       ...) {
